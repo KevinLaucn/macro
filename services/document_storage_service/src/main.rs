d61ad8b97464da352094ef7a4f6fb115555c8213
Kevin Lau <chnprint@foxmail.com>
2026-09-04 20:47:17 +0800

    fix(dss): add module declarations to document_storage_service main dispatcher

--- services/document_storage_service/src/main.rs
+++ services/document_storage_service/src/main.rs
@@ -1,13 +1,23 @@
 #![recursion_limit = "256"]
 
 #[cfg(feature = "full-product")]
+mod api;
+mod config;
+#[cfg(feature = "full-product")]
 mod full_main;
 #[cfg(not(feature = "full-product"))]
 mod lean_main;
+#[cfg(feature = "full-product")]
+mod model;
+#[cfg(feature = "full-product")]
+mod outbound;
+#[cfg(feature = "full-product")]
+mod service;
 
 #[cfg(feature = "full-product")]
-fn main() -> anyhow::Result<()> {
-    full_main::main()
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    full_main::run().await
 }
 
 #[cfg(not(feature = "full-product"))]
